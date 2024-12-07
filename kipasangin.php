8634ce1dbceb8afc92851df734efbcaded1a40e6
firman <45849550+firmantsani@users.noreply.github.com>
2024-12-07 07:33:51 +0700

    Fix

--- kipasangin.php
+++ kipasangin.php
@@ -4,7 +4,7 @@ function generateRandomDeviceId($length = 16) {
     $characters = '0123456789abcdef';
     $charactersLength = strlen($characters);
     $randomString = '';
-    for ($i = 0; i < $length; $i++) {
+    for ($i = 0; $i < $length; $i++) {
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
